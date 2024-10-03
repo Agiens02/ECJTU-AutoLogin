8fb1beac09d9b164db50f991aede151579f26da1
Agiens02 <125732107+Agiens02@users.noreply.github.com>
2024-10-03 10:36:44 +0800

    优化自动登录

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lonx.ecjtu.autologin"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2498
-        versionName = "1.1"
+        versionCode = 2500
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
